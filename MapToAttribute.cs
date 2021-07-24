517725047b9d17f2fad01c942abdec066f32b6b2
勇 孙 <sy_609@163.com>
2021-07-24 23:11:50 +0800

    添加项目文件。

---
+++ MapToAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloCat
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+    public class MapToAttribute : Attribute
+    {
+        public Type ServiceType { get; }
+        public Lifetime Lifetime { get; }
+
+        public MapToAttribute(Type serviceType, Lifetime lifetime)
+        {
+            ServiceType = serviceType;
+            Lifetime = lifetime;
+        }
+    }
+}
